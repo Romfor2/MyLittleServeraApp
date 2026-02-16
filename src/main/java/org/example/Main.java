874151566b7839117a228235b0a310df8e7c43f9
Mironenko Andrej <romfor.ru@mail.ru>
2026-02-17 03:38:52 +0700

    1.1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,14 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+        String name = "roby";
+        String fullname = "dolly rob";
+        String email = "roby777@dorry.net";
+        User user = User.create(name, fullname, email);
+        System.out.println(user.format());
+        //User user1 = User.create("r", fullname, email);
+        //User user2 = User.create("#", fullname, email);
+        //User user3 = User.create(name, fullname, "email");
     }
 }
